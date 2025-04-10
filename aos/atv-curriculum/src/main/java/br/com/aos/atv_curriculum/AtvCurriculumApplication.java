81dd15e9d57e900aba8b926873dfa158b0a60eac
Bruno Guilherme da Silva <121551639+Bruno-Guilherme@users.noreply.github.com>
2025-04-10 14:45:58 +0000

    configuração inicial do projeto spring.

---
+++ aos/atv-curriculum/src/main/java/br/com/aos/atv_curriculum/AtvCurriculumApplication.java
@@ -0,0 +1,13 @@
+package br.com.aos.atv_curriculum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvCurriculumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtvCurriculumApplication.class, args);
+	}
+
+}
---
+++ aos/atv-curriculum/src/test/java/br/com/aos/atv_curriculum/AtvCurriculumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aos.atv_curriculum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtvCurriculumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
